f3b819c7111c3c74455db59655c31e6b66e7f6f7
Tu Nombre <usuario@example.com>
2025-05-05 19:22:30 -0500

    Estructura base

---
+++ src/main/java/com/usta/startupconnect/StartUpConnectApplication.java
@@ -0,0 +1,13 @@
+package com.usta.startupconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUpConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartUpConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/usta/startupconnect/StartUpConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usta.startupconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
