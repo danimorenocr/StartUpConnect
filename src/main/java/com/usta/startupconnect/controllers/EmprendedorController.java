d6a38631cc20f9f0688bf2d2327e5381f1d43d0a
Tu Nombre <usuario@example.com>
2025-05-11 17:48:22 -0500

    Controlador + html, ya lista: usuario, convocatoria, emprendedor, entregable, etapa, evento, mentor, startup, tarea

--- src/main/java/com/usta/startupconnect/controllers/EmprendedorController.java
+++ src/main/java/com/usta/startupconnect/controllers/EmprendedorController.java
@@ -1,4 +1,29 @@
 package com.usta.startupconnect.controllers;
 
+import com.usta.startupconnect.entities.EmprendedorEntity;
+import com.usta.startupconnect.models.services.EmprendedorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Controller
 public class EmprendedorController {
+
+    @Autowired
+    private EmprendedorService emprendedorService;
+
+    @GetMapping(value = "/emprendedor")
+    public String listarEmprendedores(Model model) {
+        model.addAttribute("title", "Emprendedores");
+        model.addAttribute("urlRegistro", "/crearEmprendedor");
+        List< EmprendedorEntity> lista = emprendedorService.findAll();
+        lista.sort(Comparator.comparing(EmprendedorEntity::getDocumento));
+        model.addAttribute("emprendedores", lista);
+        return "/emprendedor/listarEmprendedores";
+    }
+
 }
--- src/main/java/com/usta/startupconnect/controllers/EventoController.java
+++ src/main/java/com/usta/startupconnect/controllers/EventoController.java
@@ -1,4 +1,35 @@
 package com.usta.startupconnect.controllers;
 
+import com.usta.startupconnect.entities.EventoEntity;
+import com.usta.startupconnect.models.services.EventoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Controller
 public class EventoController {
+    @Autowired
+    private EventoService eventoService;
+
+    @GetMapping(value = "/evento")
+    public String listarEventos(Model model) {
+        model.addAttribute("title", "Eventos");
+        model.addAttribute("urlRegistro", "/crearEvento");
+        List<EventoEntity> lista = eventoService.findAll();
+        lista.sort(Comparator.comparing(EventoEntity::getId));
+        model.addAttribute("eventos", lista);
+        return "/evento/listarEventos";
+    }
+    
+    @GetMapping(value = "/crearEvento")
+    public String crearEvento(Model model) {
+        model.addAttribute("title", "Crear Evento");
+        EventoEntity evento = new EventoEntity();
+        model.addAttribute("evento", evento);
+        return "/evento/crearEvento";
+    }
 }
