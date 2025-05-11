dafed9735c16be5c42fe6ad2dc272306b04eeb3e
Tu Nombre <usuario@example.com>
2025-05-11 17:51:38 -0500

    Controlador + html, ya lista: usuario, convocatoria, emprendedor, entregable, etapa, evento, mentor, startup, tarea

--- src/main/java/com/usta/startupconnect/controllers/UsuarioController.java
+++ src/main/java/com/usta/startupconnect/controllers/UsuarioController.java
@@ -1,10 +1,15 @@
 package com.usta.startupconnect.controllers;
 
+import com.usta.startupconnect.entities.EmprendedorEntity;
+import com.usta.startupconnect.entities.UsuarioEntity;
 import com.usta.startupconnect.models.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Comparator;
+import java.util.List;
 
 @Controller
 public class UsuarioController {
@@ -14,6 +19,16 @@ public class UsuarioController {
     @GetMapping(value = "/administrador")
     public String administrador(Model model) {
         return "/administrador/dashboardAdmin";
+    }
+
+    @GetMapping(value = "/usuario")
+    public String listarUsuarios(Model model) {
+        model.addAttribute("title", "Usuarios");
+        model.addAttribute("urlRegistro", "/crearUsuarios");
+        List<UsuarioEntity> lista = usuarioService.findAll();
+        lista.sort(Comparator.comparing(UsuarioEntity::getDocumento));
+        model.addAttribute("usuarios", lista);
+        return "/usuario/listarUsuarios";
     }
 
 }
