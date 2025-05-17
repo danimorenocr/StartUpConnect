8558bb99c4abec6f66babd8745baaf03afdab971
Tu Nombre <usuario@example.com>
2025-05-17 18:05:40 -0500

    Crear, Lsitar, Eliminar usuarios

--- src/main/java/com/usta/startupconnect/controllers/UsuarioController.java
+++ src/main/java/com/usta/startupconnect/controllers/UsuarioController.java
@@ -1,13 +1,31 @@
 package com.usta.startupconnect.controllers;
 
-import com.usta.startupconnect.entities.EmprendedorEntity;
+import com.usta.startupconnect.entities.RolEntity;
 import com.usta.startupconnect.entities.UsuarioEntity;
+import com.usta.startupconnect.models.services.RolService;
 import com.usta.startupconnect.models.services.UsuarioService;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
 
@@ -15,6 +33,8 @@ import java.util.List;
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
+    @Autowired
+    private RolService rolService;
 
     @GetMapping(value = "/administrador")
     public String administrador(Model model) {
@@ -24,11 +44,141 @@ public class UsuarioController {
     @GetMapping(value = "/usuario")
     public String listarUsuarios(Model model) {
         model.addAttribute("title", "Usuarios");
-        model.addAttribute("urlRegistro", "/crearUsuarios");
+        model.addAttribute("crearUsuario", "/crearUsuario");
         List<UsuarioEntity> lista = usuarioService.findAll();
         lista.sort(Comparator.comparing(UsuarioEntity::getDocumento));
         model.addAttribute("usuarios", lista);
         return "/usuario/listarUsuarios";
     }
+
+    @GetMapping(value = "/crearUsuario")
+    public String crearUsuario(Model model) {
+        List<RolEntity> roles = rolService.findAll();
+        model.addAttribute("roles", roles);
+        model.addAttribute("title", "Crear usuario");
+        model.addAttribute("usuario", new UsuarioEntity());
+        return "/usuario/formCrearUsuario";
+    }
+
+    @PostMapping(value = "/crearUsuario")
+    public String guardarUsuario(@Valid UsuarioEntity usuario, @RequestParam(value = "foto") MultipartFile foto, BindingResult result, RedirectAttributes redirectAttributes) {
+        String urlImagen = guardarImagen(foto);
+        if (result.hasErrors()) {
+            System.out.println(result.getAllErrors());
+            return "usuario/crearUsuario";
+
+        }
+
+        usuario.setFotoUrl(urlImagen);
+        usuario.setFecha_creacion(LocalDate.now());
+        usuarioService.save(usuario);
+        redirectAttributes.addFlashAttribute("mensajeExito", "Room Saved Successfully");
+        return "redirect:/usuario";
+    }
+
+    private String guardarImagen(MultipartFile imagen) {
+        try {
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            HttpPost httpPost = new HttpPost("https://api.imgbb.com/1/upload");
+
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            builder.addTextBody("key", "dc172a14793a0bf9b87a96f1f2e5b4be", ContentType.TEXT_PLAIN);
+
+            builder.addBinaryBody("image", imagen.getInputStream(), ContentType.DEFAULT_BINARY, imagen.getOriginalFilename());
+
+            HttpEntity multipart = builder.build();
+            httpPost.setEntity(multipart);
+
+            HttpResponse response = httpClient.execute(httpPost);
+            HttpEntity responseEntity = response.getEntity();
+
+            if (response.getStatusLine().getStatusCode() == 200) {
+                String responseString = EntityUtils.toString(responseEntity);
+                JSONObject jsonResponse = new JSONObject(responseString);
+                boolean success = jsonResponse.getBoolean("success");
+
+                if (success) {
+                    JSONObject data = jsonResponse.getJSONObject("data");
+                    return data.getString("url");
+                }else{
+                    System.err.println("Error loading image: " + jsonResponse.optString("error", "Unknown Error"));
+                }
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+
+    @PostMapping(value = "/eliminarUsuario/{id}")
+    public String eliminarHabitacion(@PathVariable(value = "id") Long id, RedirectAttributes redirectAttributes) {
+        System.out.println("Intentando eliminar habitación con ID: " + id);
+
+        if (id > 0) {
+            UsuarioEntity usuario = usuarioService.findById(id);
+            if (usuario != null) {
+                System.out.println("usuario encontrada: " + usuario.getDocumento());
+
+
+
+                usuarioService.deleteById(id);
+                System.out.println("usuario eliminada correctamente.");
+                redirectAttributes.addFlashAttribute("success", "user deleted successfully");
+            } else {
+                System.out.println("Error: No se encontró la usuario con ID: " + id);
+                redirectAttributes.addFlashAttribute("error", "user not found");
+            }
+        } else {
+            System.out.println("Error: ID inválido.");
+            redirectAttributes.addFlashAttribute("error", "Invalid ID");
+        }
+
+        return "redirect:/usuario";
+    }
+
+    @GetMapping(value = "/editarUsuario/{id}")
+    public String editarUsuario(Model model, @PathVariable(value = "id") Long id) {
+        UsuarioEntity usuario = usuarioService.findById(id);
+        List<RolEntity> roles = rolService.findAll();
+        model.addAttribute("roles", roles);
+        model.addAttribute("title", "Editar Usuario");
+        model.addAttribute("usuarioEditar", usuario);
+        model.addAttribute("imagen", usuario.getFotoUrl());
+        return "usuario/editarUsuario";
+    }
+
+//    @PostMapping("/editarUsuario")
+//    public String actualizarUsuario(@Valid UsuarioEntity usuario,
+//                                    @RequestParam(value = "foto", required = false) MultipartFile foto,
+//                                    BindingResult result,
+//                                    RedirectAttributes redirectAttributes) {
+//
+//        if (result.hasErrors()) {
+//            System.out.println(result.getAllErrors());
+//            return "usuario/editarUsuario";
+//        }
+//
+//        // Si hay una nueva foto, la procesamos
+//        if (foto != null && !foto.isEmpty()) {
+//            String urlImagen = guardarImagen(foto);
+//            usuario.setFotoUrl(urlImagen);
+//        } else {
+//            // Mantener la foto anterior si no se envió una nueva
+//            UsuarioEntity usuarioExistente = usuarioService.findById(usuario.);
+//            if (usuarioExistente != null) {
+//                usuario.setFotoUrl(usuarioExistente.getFotoUrl());
+//                usuario.setFecha_creacion(usuarioExistente.getFecha_creacion()); // preservar fecha original
+//            }
+//        }
+//
+//        usuarioService.save(usuario);
+//        redirectAttributes.addFlashAttribute("mensajeExito", "Usuario actualizado exitosamente");
+//        return "redirect:/usuario";
+//    }
+
+
+
 
 }
--- src/main/java/com/usta/startupconnect/models/services/UsuarioService.java
+++ src/main/java/com/usta/startupconnect/models/services/UsuarioService.java
@@ -9,4 +9,9 @@ import java.util.List;
 public interface UsuarioService {
     List<UsuarioEntity> findAll();
 
+    public void save(UsuarioEntity usuario);
+
+    public UsuarioEntity findById(Long id);
+
+    public void deleteById(Long id);
 }
