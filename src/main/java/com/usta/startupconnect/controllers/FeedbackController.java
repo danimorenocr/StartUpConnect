6f7cc581701c132d81d5754575b23023c580c391
Sofi-06 <mariasofiatm@gmail.com>
2025-06-03 16:34:02 -0500

    detallitoos fedback, render

--- src/main/java/com/usta/startupconnect/controllers/FeedbackController.java
+++ src/main/java/com/usta/startupconnect/controllers/FeedbackController.java
@@ -176,6 +176,6 @@ public class FeedbackController {
         }
         feedbackService.save(feedback);
         redirectAttributes.addFlashAttribute("mensajeExito", "¡Calificación de mentor guardada correctamente!");
-         return "etapa/listarEtapasEmprendedor";
+        return "redirect:/etapasEmprendedor";
     }
 }
